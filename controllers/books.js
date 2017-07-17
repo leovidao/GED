c391cd8b97969ff457862184f0e72dddab302811
leonardo.machado <leonardo.machado@labtrans.ufsc.br>
2017-07-16 23:22:53 -0300

    API, estrutura inicial, contruída a partir do material do Waldemar Neto.
    https://www.youtube.com/watch?v=l4glc0XNVbM

---
+++ controllers/books.js
@@ -0,0 +1,52 @@
+import httpStatus from 'http-status';
+
+const defaultResponse = (data, statusCode = httpStatus.OK) => ({
+  data,
+  statusCode,
+});
+
+const errorResponse = (message, statusCode = httpStatus.BAD_REQUEST) => defaultResponse({
+  error: message,
+}, statusCode);
+
+class BooksController {
+  constructor(Books) {
+    this.Books = Books;
+  }
+
+  getAll() {
+    return this.Books.findAll({})
+      .then(result => defaultResponse(result))
+      .catch(error => errorResponse(error.message));
+  }
+
+  getById(params) {
+    return this.Books.findOne({ where: params })
+      .then(result => defaultResponse(result))
+      .catch(error => errorResponse(error.message));
+  }
+
+  create(data) {
+    return this.Books.create(data)
+      .then(result => defaultResponse(result, httpStatus.CREATED))
+      .catch(error => errorResponse(error.message, httpStatus.UNPROCESSABLE_ENTITY));
+  }
+
+  update(data, params) {
+    return this.Books.update(data, {
+      where: params,
+    })
+      .then(result => defaultResponse(result))
+      .catch(error => errorResponse(error.message, httpStatus.UNPROCESSABLE_ENTITY));
+  }
+
+  delete(params) {
+    return this.Books.destroy({
+      where: params,
+    })
+      .then(result => defaultResponse(result, httpStatus.NO_CONTENT))
+      .catch(error => errorResponse(error.message, httpStatus.UNPROCESSABLE_ENTITY));
+  }
+}
+
+export default BooksController;
---
+++ controllers/users.js
@@ -0,0 +1,52 @@
+import httpStatus from 'http-status';
+
+const defaultResponse = (data, statusCode = httpStatus.OK) => ({
+  data,
+  statusCode,
+});
+
+const errorResponse = (message, statusCode = httpStatus.BAD_REQUEST) => defaultResponse({
+  error: message,
+}, statusCode);
+
+class UsersController {
+  constructor(Users) {
+    this.Users = Users;
+  }
+
+  getAll() {
+    return this.Users.findAll({})
+      .then(result => defaultResponse(result))
+      .catch(error => errorResponse(error.message));
+  }
+
+  getById(params) {
+    return this.Users.findOne({ where: params })
+      .then(result => defaultResponse(result))
+      .catch(error => errorResponse(error.message));
+  }
+
+  create(data) {
+    return this.Users.create(data)
+      .then(result => defaultResponse(result, httpStatus.CREATED))
+      .catch(error => errorResponse(error.message, httpStatus.UNPROCESSABLE_ENTITY));
+  }
+
+  update(data, params) {
+    return this.Users.update(data, {
+      where: params,
+    })
+      .then(result => defaultResponse(result))
+      .catch(error => errorResponse(error.message, httpStatus.UNPROCESSABLE_ENTITY));
+  }
+
+  delete(params) {
+    return this.Users.destroy({
+      where: params,
+    })
+      .then(result => defaultResponse(result, httpStatus.NO_CONTENT))
+      .catch(error => errorResponse(error.message, httpStatus.UNPROCESSABLE_ENTITY));
+  }
+}
+
+export default UsersController;
